77c28719b5710555807fd00375b0f80e0b1f5255
Trevor Copeland <tcopeland@dg3n.net>
2013-12-08 13:38:27 -0800

    added lecture links, added other links, added homework and footer, updated css and js to accomodate

--- js/func.js
+++ js/func.js
@@ -3,8 +3,18 @@ $(document).ready(function() {
 
   $(window).scroll(function(){
   	navWidth = determineNavWidth();
-
-  	if ( $(window).scrollTop() >= 255 ) {
+    
+    if ($("#lecture_list ul").is(":visible") && $("#other_links ul").is(":visible")) {
+      fixedPoint = 877;
+    } else if($("#lecture_list ul").is(":hidden") && $("#other_links ul").is(":visible")) {
+      fixedPoint = 469;
+    } else if ($("#lecture_list ul").is(":visible") && $("#other_links ul").is(":hidden")) {
+      fixedPoint = 671;
+    } else {
+      fixedPoint = 263;
+    }
+    
+  	if ( $(window).scrollTop() >= fixedPoint ) {
   		$("#lecture_nav").css({
   			position: 'fixed',
   			top: '10px',
@@ -22,12 +32,20 @@ $(document).ready(function() {
   	$(window).trigger('scroll');
   });
 
+  $("a[href='#other_links']").click(function(){
+    $("#other_links ul").toggle(0);
+    return false;
+  });
+  $("a[href='#lecture_list']").click(function(){
+    $("#lecture_list ul").toggle(0);
+    return false;
+  });
 });
 
 function determineNavWidth() {
-	if ($(window).width() >= 992 && $(window).width() < 1200) {
+	if ($(window).width() >= 992 && $(window).width() < 1184) {
 		return '132px';
-	} else if ($(window).width() >= 1200) {
+	} else if ($(window).width() >= 1184) {
 		return '165px';
 	}
 };
